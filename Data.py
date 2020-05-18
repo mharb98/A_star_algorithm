cfbc6d9e7e8f0d501399ff1b1451efd05e6c4fe6
marwan harb <marwanharb65@outlook.com>
2020-05-18 02:27:30 +0200

    Added function in Data.py

--- Data.py
+++ Data.py
@@ -6,8 +6,8 @@ from TimeTable import TimeTable
 class Data:
     City1 = City("Cairo",123,456)
     City2 = City("Casablanca",678,856)
-    Days1 = ["Tue","Fri"]
-    Days2 = ["Sun","Wed"]
+    Days1 = ["tue","fri"]
+    Days2 = ["sun","wed"]
     Flight1 = Flight("11:30","12:40",1,Days1)
     Flight2 = Flight("1:30","3:30",2,Days2)
 
@@ -43,6 +43,64 @@ class Data:
         dummy2 = abs(int(time1_list[1]) - int(time2_list[1]))/60
 
         return dummy1 + dummy2
+
+    def mapDays(self,Day):
+        if Day == "Sunday":
+            return "sun"
+        elif Day == "Monday":
+            return "mon"
+        elif Day == "Tuesday":
+            return "tue"
+        elif Day == "Wednesday":
+            return "wed"
+        elif Day == "Thursday":
+            return "thu"
+        elif Day == "Friday":
+            return "fri"
+        else:
+            return "sat"
+
+    def getRange(self,Day1,Day2):
+        daysArray = ["sun","mon","tue","wed","thu","fri","sat"]
+        index1 = daysArray.index(Day1)
+        index2 = daysArray.index(Day2)
+        retList = []
+        retList2 = []
+        if index1 == index2:
+            retList = [Day1]
+            return retList
+        elif index1 < index2:
+            for i in range(len(daysArray)):
+                if i>=index1 and i<=index2:
+                    retList.append(daysArray[i])
+            return retList
+        else:
+            for i in range(len(daysArray)):
+                if i>=index2 and i<=index1:
+                    retList.append(daysArray[i])
+            retList2.append(Day1)
+            for i in range(len(daysArray)):
+                if daysArray[i] not in retList:
+                    retList2.append(daysArray[i])
+            retList2.append(Day2)
+            return retList2
+
+    def getBeforeAfter(self,Day):
+        if Day == "sat":
+            return ["fri","sun"]
+        elif Day == "sun":
+            return ["sat","mon"]
+        elif Day == "mon":
+            return ["sun","tue"]
+        elif Day == "tue":
+            return ["mon","wed"]
+        elif Day == "wed":
+            return ["tue","thu"]
+        elif Day == "thu":
+            return ["wed","fri"]
+        elif Day == "fri":
+            return ["thu","sat"]
+
 
 
 
--- Main.py
+++ Main.py
@@ -7,7 +7,8 @@ City2 = Data.City2
 
 Data1 = Data()
 
-print(Data1.calculateTime(Flight11))
-print(Data1.calculateTime(Flight22))
-print(Data1.calculateDistance(City1,City2))
+print(Data1.getRange("tue","fri"))
+
+
+
 
