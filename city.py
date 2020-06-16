fed1a7e07097ec9de8426b3380d67b88fb6f3830
Mohammad Ashraf <g.mohammad.ashraf.99@gmail.com>
2020-06-16 14:25:37 +0200

    removing buggy files

--- city.py
+++
@@ -1,8 +0,0 @@
-
-class City:
-
-    def __init__(self, name, longitude, latitude):
-        self.name = name
-        self.longitude = longitude
-        self.latitude = latitude
-
--- flight.py
+++
@@ -1,8 +0,0 @@
-
-class Flight:
-
-    def __init__(self, departureTime, arrivalTime, number, days):
-        self.departureTime = departureTime
-        self.arrivalTime = arrivalTime
-        self.number = number
-        self.days = days
